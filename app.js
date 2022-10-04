6b32b9e445839df7372cde54a499fb9254ca7efe
Tarnished <npwebapps@gmail.com>
2022-10-04 06:42:52 -0500

    x

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const morgan = require('morgan');
-
-
+const mongoose = require('mongoose');
 
 const app = express();
 
-const dbURI = 'mongodb+srv://ninj:test1234@cluster0.schqpii.mongodb.net/?retryWrites=true&w=majority'
-app.set('view engine', 'ejs');
+const dbURI = 'mongodb+srv://ninj:dq3beK7QGFxCEcMu@cluster0.schqpii.mongodb.net/finalP?retryWrites=true&w=majority'
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then((result) => app.listen(4001))  
+.catch((err) => console.log(err));
 
-app.listen(3000);
+app.set('view engine', 'ejs'); 
+
+
 
 app.use(express.static('public'));
 app.use(morgan('dev'));
@@ -72,4 +75,4 @@ res.render('create', {title: 'Create a new blog'});
     
     app.use((req, res) => {
         res.status(404).render('404', {title: '404'});
-    }) */
+    }) */ 
