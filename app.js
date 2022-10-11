05ab40ae81431d05bd3f51b827122a67afe0bbb8
Tarnished <npwebapps@gmail.com>
2022-10-11 17:46:23 -0500

    x

--- app.js
+++ app.js
@@ -1,65 +1,40 @@
-const express = require('express');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
-const blogRoutes = require('./routes/blogRoutes');
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const blogRoutes = require("./routes/blogRoutes");
+const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
 const app = express();
 
-const dbURI = 'mongodb+srv://ninj:dq3beK7QGFxCEcMu@cluster0.schqpii.mongodb.net/finalP?retryWrites=true&w=majority'
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-.then((result) => app.listen(4001))  
-.catch((err) => console.log(err));
+const dbURI =
+  "mongodb+srv://ninj:dq3beK7QGFxCEcMu@cluster0.schqpii.mongodb.net/finalP?retryWrites=true&w=majority";
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(4001))
+  .catch((err) => console.log(err));
 
-app.set('view engine', 'ejs'); 
-
-
-
-app.use(express.static('public'));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan('dev'));
+app.use(morgan("dev"));
+app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-app.get('/', (req, res) => {
-    
-    res.render('index');
-})
+app.get("/schedule", (req, res) => {
+  res.render("schedule");
+});
 
-
-    
-app.get('/schedule', (req, res) => {
-    
-    res.render('schedule');
-})
-
-app.get('/mySchedule', (req, res) => {
-    
-    res.render('/classes', { title: 'My Schedule' });
-})
-
+app.get("/mySchedule", (req, res) => {
+  res.render("/classes", { title: "My Schedule" });
+});
 
 app.use(blogRoutes);
-// 404 page
+app.use(authRoutes);
+
 app.use((req, res) => {
-    res.status(404).render('404', { title: '404' });
-  });
-/*
-    app.get('/about', (req, res) => {
-        res.render('about', {title: 'About'});
-    })
-
-
-    app.get('blogs/create', (req, res) => {
-res.render('create', {title: 'Create a new blog'});
-    })
-
-    app.get('blogs/create', (req, res) => {
-        res.render('create', {title: 'Create a new blog'});
-            })
-
-            app.get('blogs/create', (req, res) => {
-                res.render('create', {title: 'Create a new blog'});
-                    })
-                
-    
-    app.use((req, res) => {
-        res.status(404).render('404', {title: '404'});
-    }) */ 
+  res.status(404).render("404", { title: "404" });
+});
