e467ffa8acc15c81d2ffbe6561d75ef62f0b43d5
Tarnished <npwebapps@gmail.com>
2022-10-11 18:47:15 -0500

    x

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,6 +5,14 @@ const handleErrors = (err) => {
   console.log(err.message, err.code);
   let errors = { email: "", password: "" };
 
+  if (err.message === "incorrect email") {
+    errors.email = "That email is not registered";
+  }
+
+  if (err.message === "incorrect password") {
+    errors.email = "That password is incorrect";
+  }
+
   if (err.code === 11000) {
     errors.email = "That email is already registered";
     return errors;
@@ -50,7 +58,13 @@ module.exports.signup_post = async (req, res) => {
 module.exports.login_post = async (req, res) => {
   const { email, password } = req.body;
 
-  console.log(email, password);
-
-  res.send("user login");
+  try {
+    const user = await User.login(email, password);
+    const token = createToken(user._id);
+    res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.status(200).json({ user: user._id });
+  } catch (err) {
+    const errors = handleErrors(err);
+    res.status(400).json({ errors });
+  }
 };
